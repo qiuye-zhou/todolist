98382bd29e08ba5980dad532e3270b3fc0ae801e
qiuye <1302580865@qq.com>
2022-12-05 11:03:54 +0800

    updata: add bg

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import 'element-plus/dist/index.css';
+import './assets/style.css';
 
 createApp(App).mount('#app');
